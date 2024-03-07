e17c4d147222b1cb61f2ad9da70918d471f8d107
Gon Solo <gonsolo@gmail.com>
2024-03-07 10:49:29 +0100

    Descriptor set skeleton.

---
+++ src/borg/vulkan/borg_descriptor_set.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright © 2024 Andreas Wendleder
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "borg_entrypoints.h"
+
+VKAPI_ATTR VkResult VKAPI_CALL
+borg_AllocateDescriptorSets(VkDevice device,
+                            const VkDescriptorSetAllocateInfo *pAllocateInfo,
+                            VkDescriptorSet *pDescriptorSets)
+{
+   // TODO
+
+   return VK_SUCCESS;
+}
+
+VKAPI_ATTR VkResult VKAPI_CALL
+borg_CreateDescriptorPool(VkDevice _device,
+                           const VkDescriptorPoolCreateInfo *pCreateInfo,
+                           const VkAllocationCallbacks *pAllocator,
+                           VkDescriptorPool *pDescriptorPool)
+{
+   // TODO
+   return VK_SUCCESS;
+}
+
+VKAPI_ATTR void VKAPI_CALL
+borg_UpdateDescriptorSets(VkDevice device,
+                          uint32_t descriptorWriteCount,
+                          const VkWriteDescriptorSet *pDescriptorWrites,
+                          uint32_t descriptorCopyCount,
+                          const VkCopyDescriptorSet *pDescriptorCopies)
+{
+   // TODO
+}
