ae0d5cb76784b9eee36f5b4715050bf2caddf4eb
Gon Solo <gonsolo@gmail.com>
2024-03-08 12:20:58 +0100

    Driver submit.

--- src/borg/vulkan/borg_queue.c
+++ src/borg/vulkan/borg_queue.c
@@ -6,6 +6,13 @@
 #include "borg_device.h"
 #include "borg_queue.h"
 
+static VkResult borg_driver_submit(struct vk_queue *queue,
+                            struct vk_queue_submit *submit)
+{
+   // TODO
+   return VK_SUCCESS;
+}
+
 VkResult borg_queue_init(struct borg_device* dev,
 			 struct borg_queue *queue,
 			 const VkDeviceQueueCreateInfo *pCreateInfo,
@@ -16,6 +23,8 @@ VkResult borg_queue_init(struct borg_device* dev,
    result = vk_queue_init(&queue->vk, &dev->vk, pCreateInfo, index_in_family);
    if (result != VK_SUCCESS)
       return result;
+
+   queue->vk.driver_submit = borg_driver_submit;
 
    return result;
 }
