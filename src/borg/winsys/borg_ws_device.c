fa8cc2f397c9dc832ff188a9f17f83909a6c3de2
Gon Solo <gonsolo@gmail.com>
2024-06-24 11:54:10 +0200

    Flesh out borg_ws_device_new.

--- src/borg/winsys/borg_ws_device.c
+++ src/borg/winsys/borg_ws_device.c
@@ -2,16 +2,37 @@
 
 #include "util/u_memory.h"
 
+#include <fcntl.h>
 #include <xf86drm.h>
 
 struct borg_ws_device *
 borg_ws_device_new(drmDevicePtr drm_device)
 {
-        // TODO
+        const char *path = drm_device->nodes[DRM_NODE_RENDER];
         struct borg_ws_device *device = CALLOC_STRUCT(borg_ws_device);
+        drmVersionPtr ver = NULL;
 
-        // TODO
+        int fd = open(path, O_RDWR | O_CLOEXEC);
+        if (fd < 0)
+                goto out_open;
+
+        ver = drmGetVersion(fd);
+        if (!ver)
+                goto out_err;
+
+        if (strncmp("borg", ver->name, ver->name_len) != 0) {
+                fprintf(stderr,
+                        "DRM kernel driver '%.*s' in use. Borg requires borg.\n",
+                        ver->name_len, ver->name);
+                goto out_err;
+        }
+
         simple_mtx_init(&device->bos_lock, mtx_plain);
-        // TODO
+out_err:
+        if (ver)
+                drmFreeVersion(ver);
+out_open:
+        FREE(device);
+        close(fd);
         return NULL;
 }
