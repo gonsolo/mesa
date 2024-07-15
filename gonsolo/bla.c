91052b0695953ba1131c5d60fc0226741c9e5ba2
Gon Solo <gonsolo@gmail.com>
2024-07-15 10:01:00 +0200

    Remove bla.c, ct shortcut.

--- gonsolo/bla.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "xf86drm.h"
-
-int main() {
-        drmDevicePtr devices[256];
-        int max_devices = drmGetDevices2(0, devices, 256);
-        printf("%i\n", max_devices);
-}
-
