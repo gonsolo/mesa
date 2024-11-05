ec3366c3ba3482895b2ef6a20ee66196c228c396
Gon Solo <gonsolo@gmail.com>
2024-11-05 17:52:41 +0100

    Begin encoding.

--- src/borg/compiler/bak/sm.rs
+++ src/borg/compiler/bak/sm.rs
@@ -1,7 +1,7 @@
 // Copyright © 2024 Andreas Wendleder
 // SPDX-License-Identifier: MIT
 
-use crate::ir::{Op, Shader};
+use crate::ir::*;
 
 struct SMEncoder {
     #[allow(dead_code)]
@@ -9,13 +9,27 @@ struct SMEncoder {
     inst: [u32; 4]
 }
 
+impl SMEncoder {
+    fn set_opcode(&mut self, opcode: u16) {
+        // TODO
+    }
+}
+
 trait SMOp {
     fn encode(&self, e: &mut SMEncoder);
+}
+
+impl SMOp for OpMov {
+
+    fn encode(&self, e: &mut SMEncoder) {
+        e.set_opcode(0x666)
+    }
 }
 
 macro_rules! as_sm_op_match {
     ($op: expr) => {
         match $op {
+            Op::Mov(op) => op,
             _ => panic!("Unsupported op: {}", $op),
         }
     }
