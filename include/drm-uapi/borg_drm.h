6a499e67701c2b4abb06188d5b1ec7380b28013a
Gon Solo <gonsolo@gmail.com>
2025-01-28 18:22:09 +0100

    New borg_drm.h.

--- include/drm-uapi/borg_drm.h
+++ include/drm-uapi/borg_drm.h
@@ -23,9 +23,14 @@ struct drm_borg_gem_new {
         struct drm_borg_gem_info info;
 };
 
-struct drm_borg_vm_init {
-        __u64 kernel_managed_addr;
-        __u64 kernel_managed_size;
+struct drm_borg_getparam {
+        __u64 param;
+        __u64 value;
+};
+
+struct drm_borg_vm_bind {
+   __u32 op_count;
+   __u64 op_ptr;
 };
 
 struct drm_borg_vm_bind_op {
@@ -36,27 +41,24 @@ struct drm_borg_vm_bind_op {
    __u64 range;
 };
 
-struct drm_borg_vm_bind {
-   __u32 op_count;
-   __u64 op_ptr;
+struct drm_borg_vm_init {
+        __u64 kernel_managed_addr;
+        __u64 kernel_managed_size;
 };
+
 
 #define DRM_BORG_VM_INIT            0x10
 #define DRM_BORG_VM_BIND            0x11
 #define DRM_BORG_GEM_NEW            0x40
+#define DRM_BORG_GETPARAM           0x51
 
-#define DRM_IOCTL_BORG_VM_INIT            DRM_IOWR(DRM_COMMAND_BASE + DRM_BORG_VM_INIT, struct drm_borg_vm_init)
-#define DRM_IOCTL_BORG_VM_BIND            DRM_IOWR(DRM_COMMAND_BASE + DRM_BORG_VM_BIND, struct drm_borg_vm_bind)
 #define DRM_IOCTL_BORG_GEM_NEW            DRM_IOWR(DRM_COMMAND_BASE + DRM_BORG_GEM_NEW, struct drm_borg_gem_new)
+#define DRM_IOCTL_BORG_GETPARAM           DRM_IOWR(DRM_COMMAND_BASE + DRM_BORG_GETPARAM, struct drm_borg_getparam)
+#define DRM_IOCTL_BORG_VM_BIND            DRM_IOWR(DRM_COMMAND_BASE + DRM_BORG_VM_BIND, struct drm_borg_vm_bind)
+#define DRM_IOCTL_BORG_VM_INIT            DRM_IOWR(DRM_COMMAND_BASE + DRM_BORG_VM_INIT, struct drm_borg_vm_init)
 
 #define BORG_GETPARAM_STATUS        0
 
-#define DRM_BORG_GETPARAM           0x01
-
-struct drm_borg_getparam {
-        __u64 param;
-        __u64 value;
-};
 
 #if defined(__cplusplus)
 }
